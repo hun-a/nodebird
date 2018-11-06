1111bd567713ec7434de8aa95fda3fd466d8d1c7
seunghun <seunghun.chan@gmail.com>
2018-11-07 00:55:23 +0900

    add the new router - page

---
+++ routes/page.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/profile', (req, res) => {
+  res.render('profile', { title: 'My info - NodeBird', user: null });
+});
+
+router.get('/join', (req, res) => {
+  res.render('join', {
+    title: 'Sign Up - NodeBird',
+    user: null,
+    joinError: req.flash('joinError')
+  });
+});
+
+router.get('/', (req, res, next) => {
+  res.render('main', {
+    title: 'NodeBird',
+    twits: [],
+    user: null,
+    loginError: req.flash('loginError')
+  });
+});
+
+module.exports = router;
